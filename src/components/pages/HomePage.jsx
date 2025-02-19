c8df566d78bcc757310fe29ca89f359463166a87
estherbg85 <estherbonillagirol85@gmail.com>
2025-02-19 20:31:33 +0100

    imageUrl books

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,15 +1,38 @@
-import Header from '../layout/Header';
-function HomePage () {
-    return (
-        <div>
-          <Header/>
-          <section>
-            <h2 className="form">¿Buscas un libro en concreto?</h2>
-            
-            <p>Si no lo encuentras, ponte en contacto conmigo y lo solucionaremos</p>
-          </section>
-        </div>
-    );
+import Header from "../layout/Header";
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <section>
+        <h2 className="form">¿Buscas un libro en concreto?</h2>
+        <form className="form">
+          <div>
+            <label className="form_filter" htmlFor="name">
+              Titulo
+            </label>
+            <input
+              className="form_title"
+              autoComplete="off"
+              type="search"
+              name="title"
+            />
+            <label className="form_filter" htmlFor="autor">
+              Autor
+            </label>
+            <select
+              className="form_autor"
+              id="autorFilter"
+              placeholder="Autor"
+            ></select>
+          </div>
+        </form>
+        <p>
+          Si no lo encuentras, ponte en contacto conmigo y lo solucionaremos
+        </p>
+        <button className="btnContact">Contacto</button>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
